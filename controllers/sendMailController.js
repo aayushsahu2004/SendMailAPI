8c01f48e9a9dd8608041e782822332f8356d8753
aayushsahu2004 <sahuaayush2999@gmail.com>
2024-06-07 22:57:34 +0530

    changes

--- controllers/sendMailController.js
+++ controllers/sendMailController.js
@@ -11,7 +11,7 @@ exports.homePage = async function (req, res, next) {
 exports.subscribeSendMail = async function (req, res, next) {
     try {
         sendSubcribeMail(req.body.email)
-        res.status(200).json({ message: "Subscribe Successfully!", email: req.body.email});
+        res.status(200).json({ message: "Subscribe Successfully!" });
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
