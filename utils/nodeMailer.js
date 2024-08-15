47b27cf832deb52043ebd895219eb3604f8a37bf
aayushsahu2004 <sahuaayush2999@gmail.com>
2024-08-15 16:39:53 +0530

    change

--- utils/nodeMailer.js
+++ utils/nodeMailer.js
@@ -17,7 +17,7 @@ exports.sendSubcribeMail = (email) => {
         subject: "Welcome to Medhya Technologies!",
         html: `<div style="font-family: Arial, sans-serif; max-width: 600px; margin: auto; padding: 20px; border: 1px solid #e0e0e0; border-radius: 10px; background-color: #ffffff;">
         <div style="text-align: center; padding-bottom: 20px;">
-            <img src="https://ik.imagekit.io/xfssqj13p/Medhya%20full%20logo2.png?updatedAt=1723719934167" alt="Medhya Technologies" style="width: 16vw; height:4vw; object-fit: cover; background-color:red;"/>
+            <img src="https://ik.imagekit.io/xfssqj13p/Medhya%20full%20logo2.png?updatedAt=1723719934167" alt="Medhya Technologies" style="width: 16vw; height:4vw; object-fit: cover;"/>
         </div>
         <h1 style="color: #FF9900; text-align: center;">Welcome to Medhya Technologies!</h1>
         <p style="font-size: 14px; color: #555555;">Dear Subscriber,</p>
